a4c7c724c7e415f5c96b9aaa614a96fb17501b84
Franck Oculi <100290322+FranckOculi@users.noreply.github.com>
2023-06-08 09:14:46 +0200

    Wind Update : migrate to ts

--- src/component/Wind.tsx
+++ src/component/Wind.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
 import WindIcon from './WindIcon';
+import { Wind as WindType } from '../types/weather';
 
-const Wind = ({ wind }) => {
+type Props = {
+  wind: WindType
+}
+
+const Wind = ({ wind }: Props) => {
   const windSpeed = (wind.speed * 3.6).toFixed(0);
 
   return (
     <div id='wind'>
       <div id='wind-direction'>
-        <WindIcon wind={wind.deg} />
+        <WindIcon deg={wind.deg} />
       </div>
       <div id='wind-speed'>{windSpeed + ' km/h'} </div>
     </div>
